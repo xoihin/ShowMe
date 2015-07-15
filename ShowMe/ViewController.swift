ec2eb137de3add9695fbb331226da9ea317e0175
xoihin <hinxoi@hotmail.com>
2015-07-15 12:54:38 -0400

    Initial Commit

---
+++ ShowMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowMe
+//
+//  Created by Xoi's iMac on 2015-07-15.
+//  Copyright (c) 2015 Xoi's iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
